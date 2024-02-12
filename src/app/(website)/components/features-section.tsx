8bd4924bec0c68cdf6916256b2b1e316dcd73d15
Guilherme Cunha <guiscunha@gmail.com>
2024-02-12 01:40:58 -0300

    feat: add features section

---
+++ src/app/(website)/components/features-section.tsx
@@ -0,0 +1,60 @@
+import { Icons } from "@/components/icons";
+import React from "react";
+
+const features = [
+  {
+    name: "Create your family tree",
+    description: "Create your family tree by adding all the members.",
+    icon: Icons.contact,
+  },
+  {
+    name: "Add the date of birth to your family members",
+    description:
+      "Never forget a family member's birthday again. We'll notify you when this important date is coming up.",
+    icon: Icons.cake,
+  },
+  {
+    name: "Add important memories",
+    description:
+      "Eternalize important family memories and make sure they are never forgotten.",
+    icon: Icons.memory,
+  },
+];
+
+export const FeaturesSection = () => {
+  return (
+    <section id="features" className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            How to use Treemory
+          </p>
+          <p className="mt-6 text-md text-gray-600">
+            Here&apos;s how you can use Treemory to create your family tree,
+            eternalize memories and make your life easier.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary text-primary">
+                    <feature.icon
+                      className="h-6 w-6 bg-primary text-primary-foreground"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/(website)/components/hero-section.tsx
+++ src/app/(website)/components/hero-section.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 export const HeroSection: React.FC = () => {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+    <section id="hero" className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-32 sm:py-56">
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Eternalize your family
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-md text-gray-600">
             Create your family tree and eternalize memories
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -20,7 +20,7 @@ export const HeroSection: React.FC = () => {
 
             <Button variant="ghost">
               <Link
-                href="/dashboard"
+                href="#features"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
                 Learn more <span aria-hidden="true">→</span>
@@ -29,6 +29,6 @@ export const HeroSection: React.FC = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
